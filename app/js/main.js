01bf40a449b2e89a7038081e25eea56ff0a51315
kusochek_humora <kusochek.humora@gmail.com>
2020-08-26 23:33:07 +0600

    add 3 section and footer ,Final dekstop

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,26 @@
+$('.text__slider').slick({
+  arrows: false,
+  dots: true,
+  infinite: true
+});
 
-  $('.text__slider').slick({
-    arrows:false,
-    dots:true,
-    infinite: true
-  }
-  );
+$('.test__slider').slick({
+  arrows: true,
+  dots: false
+  
+
+});
+
+// $(document).ready(function () {
+//   $("#gallery").fancybox({
+//     'autoScale': 'true',
+//     'showCloseButton': 'true',
+//     'showNavArrows': 'true'
+//   });
+// })
+$('[data-fancybox="gallery"]').fancybox({
+  autoScale: true,
+  showCloseButton: true,
+  showNavArrows: true,
+  loop: true
+});
