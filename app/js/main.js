e900c0e829adb050abf0d3728b4010dec66dcbcc
kusochek_humora <$>
2020-08-29 22:47:25 +0600

    dekstop ipad mobile -final

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,56 @@
+
+
+
+
+  $('.burger__menu').click(function(event)
+  {
+    $('.menu2,.burger__menu').toggleClass('active');
+  });
+
+
 $('.text__slider').slick({
   arrows: false,
   dots: true,
-  infinite: true
 });
 
 $('.test__slider').slick({
   arrows: true,
   dots: false
+});
+$('.team__inner2').slick({
+  arrows: false,
+  dots: true,
+  responsive: [{
+      breakpoint: 940,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 706,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 631,
+      settings: {
+        
+      }
+    },
 
+  
+
+
+
+]
 
 });
-
 $('.expertise__inner2').slick({
   arrows: false,
   dots: true
-
-
 });
 
 // $(document).ready(function () {
@@ -31,3 +66,6 @@ $('[data-fancybox="gallery"]').fancybox({
   showNavArrows: true,
   loop: true
 });
+
+
+  
